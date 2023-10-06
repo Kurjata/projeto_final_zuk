dddc1e34063663a604977fb68f5957fd65567d75
Felipe Martines Kurjata <128336634+Kurjata@users.noreply.github.com>
2023-10-06 17:44:08 -0300

    Update conexao-bd.php

--- src/conexao-bd.php
+++ src/conexao-bd.php
@@ -1,6 +1,6 @@
 <?php
 try{
-$pdo = new PDO('mysql:host=localhost;dbname=zuk', 'root', 'devfull');
+$pdo = new PDO('mysql:host=localhost;dbname=zuk', 'root', '');
 }catch(PDOException $e){
     echo "Error: ".$e->getMessage();
 }
